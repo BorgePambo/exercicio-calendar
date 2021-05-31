6a0efc875a5a5c999410bc80b2d30a6049ac378c
BorgePambo <borginhopambo01@gmail.com>
2021-05-31 18:48:35 -0300

    Java Projeto

---
+++ src/javacalendar/JavaCalendar.java
@@ -0,0 +1,16 @@
+package javacalendar;
+
+import java.util.Calendar;
+
+
+public class JavaCalendar {
+
+    
+    public static void main(String[] args) {
+        
+        Calendar c = Calendar.getInstance();
+        
+        System.out.println("DATA ATUAL: " + c.getTime());
+    }
+    
+}
